a8108ab2385d3210267d9bb597640c8f17777d1b
Alex Ungurianu <iau2@st-andrews.ac.uk>
2016-11-06 08:02:57 +0000

    Make Kelsey great as always #MakeCodeGreatAgain

--- server/setup.js
+++ server/setup.js
@@ -7,6 +7,28 @@ module.exports = function() {
     location: {
       latitude: 45,
       longitude: 45
+    },
+    type: "PI",
+    data: {
+      description: "Bestest thing ever"
     }
   });
+
+  dataStore.addPuzzle(111, {
+    location: {
+      latitude: 50,
+      longitude: 2
+    },
+    type: "Question",
+    data: {
+      question: "Which is the best kelsey?",
+      choices: [
+        "Tom Kelsey",
+        "Tom Kelsey",
+        "Tom Not Kelsey",
+        "Tom Greatest Kelsey"
+      ],
+      answer:1
+    }
+  })
 }
