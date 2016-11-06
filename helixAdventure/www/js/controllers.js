3171a366d1428ab0929dadd8d60917eb71fad171
Alex Ungurianu <iau2@st-andrews.ac.uk>
2016-11-06 10:54:20 +0000

    Maps kinda work #GuyFawkes

--- helixAdventure/www/js/controllers.js
+++ helixAdventure/www/js/controllers.js
@@ -20,19 +20,16 @@ angular.module('starter.controllers', [])
       function(err) {
         alert("err: " + JSON.stringify(err));
       })
-  })
+})
 
 
-  .controller('RegCtrl', function ($scope, $localStorage) {
-    $scope.$storage = $localStorage({
+.controller('RegCtrl', function($scope, $localStorage) {
+  $scope.$storage = $localStorage({
 
-    });
+  });
 
-    $scope.registered = true;
-  })
-
-
-
+  $scope.registered = true;
+})
 
   .controller('WhyCtrl', function ($scope, $state) {
     $scope.goToChallenge = function () {
@@ -53,13 +50,16 @@ angular.module('starter.controllers', [])
 
   })
 
-
-.controller('MapCtrl', function ($scope) {
-
+.controller('MapCtrl', function($scope) {
+  $scope.$on('$ionicView.enter', function() {
+    // This example adds an animated symbol to a polyline.
     function initMap() {
-      var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 56.019879, lng: -3.756383},
-        zoom: 16,
+      var map = new google.maps.Map(document.getElementById('map-canvas'), {
+        center: {
+          lat: 56.019879,
+          lng: -3.756383
+        },
+        zoom: 14,
         mapTypeId: 'terrain'
       });
 
@@ -68,16 +68,15 @@ angular.module('starter.controllers', [])
 
 
       /*var lineSymbol = new google.maps.Marker({
-       map: map,
-       title: 'lineSymbol',
-       icon: {
-       url: "www.svgimages.com/svg-image/s6/horse--head-silhouette.svg",
-       scaledSize: new google.maps.Size(64, 64)
-       }
-       });*/
+	map: map,
+	title: 'lineSymbol',
+	icon: {
+		url: "www.svgimages.com/svg-image/s6/horse--head-silhouette.svg",
+		scaledSize: new google.maps.Size(64, 64)
+	}
+});*/
       var lineSymbol = {
-        icon: 'https://www.svgimages.com/svg-image/s6/horse--head-silhouette.svg',
-        path: google.maps.SymbolPath.FORWARD_OPEN_ARROW,
+        path: google.maps.SymbolPath.CIRCLE,
         scale: 8,
         strokeColor: '#393'
       };
@@ -88,28 +87,53 @@ angular.module('starter.controllers', [])
 
       // Create the polyline and add the symbol to it via the 'icons' property.
       var line = new google.maps.Polyline({
-        path: [{lat: 56.019879, lng: -3.756383}, {	lat: 56.019295, lng: -3.755436
+        path: [{
+          lat: 56.019879,
+          lng: -3.756383
+        }, {
+          lat: 56.019295,
+          lng: -3.755436
         }],
         map: map
       });
 
-//create a second polyline
+
+
+      //create a second polyline
       var line2 = new google.maps.Polyline({
-        path: [{lat: 56.019295, lng: -3.755436}, {	lat: 56.017183, lng: -3.7540736
+        path: [{
+          lat: 56.019295,
+          lng: -3.755436
+        }, {
+          lat: 56.017183,
+          lng: -3.7540736
         }],
         map: map
       });
 
-//create a third polyline
+
+      //create a third polyline
       var line3 = new google.maps.Polyline({
-        path: [{lat: 56.017183, lng: -3.7540736}, {	lat: 56.015252, lng: -3.757774
+        path: [{
+          lat: 56.017183,
+          lng: -3.7540736
+        }, {
+          lat: 56.015252,
+          lng: -3.757774
         }],
         map: map
       });
 
-//create a fourth polyline
+
+
+      //create a fourth polyline
       var line4 = new google.maps.Polyline({
-        path: [{lat: 56.015252, lng: -3.757774 }, {	lat: 56.012285, lng: -3.759292
+        path: [{
+          lat: 56.015252,
+          lng: -3.757774
+        }, {
+          lat: 56.012285,
+          lng: -3.759292
         }],
         map: map
       });
@@ -118,7 +142,12 @@ angular.module('starter.controllers', [])
 
       //create a fifth polyline
       var line5 = new google.maps.Polyline({
-        path: [{lat: 56.015252, lng: -3.757774 }, {	lat: 56.012285, lng: -3.759292
+        path: [{
+          lat: 56.015252,
+          lng: -3.757774
+        }, {
+          lat: 56.012285,
+          lng: -3.759292
         }],
         map: map
       });
@@ -126,52 +155,34 @@ angular.module('starter.controllers', [])
       animateSymbol([line, line2, line3, line4], lineSymbol);
     }
 
-// Use the DOM setInterval() function to change the offset of the symbol
-// at fixed intervals.
+    // Use the DOM setInterval() function to change the offset of the symbol
+    // at fixed intervals.
     function animateSymbol(lines, symbol) {
       var count = 0;
-      lines[0].set('icons',[{
+      lines[0].set('icons', [{
         icon: symbol,
         offset: '0%'
       }])
       var inter = window.setInterval(function() {
         count = count + 1;
 
-        if(count > 100) {
+        if (count > 100) {
           clearInterval(inter);
-          lines[0].set('icons',[]);
+          lines[0].set('icons', []);
           lines.shift();
           animateSymbol(lines, symbol);
-        }
-        else {
+        } else {
           var icons = lines[0].get('icons');
           icons[0].offset = (count) + '%';
           lines[0].set('icons', icons);
         }
       }, 20);
     }
-
-    function setMarkers(map, locations)
-    {
-      var image = 'https://www.svgimages.com/svg-image/s6/horse--head-silhouette.svg';
-      for (var i = 0; i < locations.length; i++)
-      {
-        var beach = locations[i];
-        var myLatLng = new google.maps.LatLng(beach[1], beach[2]);
-        var marker = new google.maps.Marker(
-          {
-            position: myLatLng,
-            map: map,
-            icon: image,
-            title: beach[0],
-            zIndex: beach[3]
-          });
-      }
-    }
-
-  })
-
-
-  .controller('AboutCtrl', function ($scope) {
-
+    initMap();
   });
+})
+
+
+.controller('AboutCtrl', function($scope) {
+
+});
