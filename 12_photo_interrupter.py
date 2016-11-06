8016b10e9662be7c0367602eb77b7baf5e314bbe
Victor <vv20@st-andrews.ac.uk>
2016-11-06 09:12:13 +0000

    adding the call for the image recognition script from the script that takes the picture

--- 12_photo_interrupter.py
+++ 12_photo_interrupter.py
@@ -2,9 +2,16 @@
 from PIL import Image
 import os
 import time
+from objectDetection import detect_colour
+import fileinput
 
 while(1):
     os.system('raspistill -o ./tempPic/tempPicture.png')
     time.sleep(1)
-    var img = Image.open('./tempPic/tempPicture')
+    img = Image.open('./tempPic/tempPicture.png')
     #run if with victors openCV code
+    detect_colour.main("./tempPic/tempPicture")
+    result = fileinput.input()
+    if (result is "true):
+        # ping server
+
