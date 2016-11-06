27e8089b5248697a685479618e5391f695ae2671
Stacey1212 <ai26@st-andrews.ac.uk>
2016-11-06 02:46:30 +0000

    added context to tabs

--- helixAdventure/www/js/controllers.js
+++ helixAdventure/www/js/controllers.js
@@ -1,9 +1,33 @@
 angular.module('starter.controllers', [])
 
-  .controller('HomeCtrl', function ($scope) {
+  .controller('HomeCtrl', function ($scope, $state) {
+    $scope.showInfo = function () {
+      $state.go("#/tab/home/info");
+    }
+
+    $scope.register = function () {
+      if ($scope.registered) {
+        $state.go("#/tab/map");
+      }
+      else {
+        $state.go("#/tab/home/register");
+      }
+
+    }
+
+  })
+
+
+  .controller('RegCtrl', function ($scope, $localStorage) {
+    $scope.$storage = $localStorage({
+
+    });
+
+    $scope.registered = true;
   })
 
   .controller('MapCtrl', function ($scope) {
+
   })
 
 
